e2b5db00bf283da41fefea61af6341934c171e3c
andriivars <andrii13@ru.is>
2014-10-18 19:12:21 +0000

    added delimiter functionality

--- src/StringCalc/StringCalculator.java
+++ src/StringCalc/StringCalculator.java
@@ -7,24 +7,23 @@ public static int Add(String text) {
 	if (text.equals("")){
 		return 0;
 	}
-	/*char delim = ',';
-	if (text.charAt(0) == text.charAt(1) & text.charAt(1) == '/'){
-		delim = text.charAt(2);
-	}*/
 		
 	if (!(text.contains(",")||text.contains("\n"))){
 		return tonumber(text);
 	}
-	else{
-		String[] numbers = text.split(",|\n");
-		int sum = 0;
-    	for(String d : numbers)
-    	{
-    		sum += tonumber(d);
-    	}
-        return sum;
-	} 
-		
+	char delim = ',';
+	if (text.charAt(0) == text.charAt(1) & text.charAt(1) == '/'){
+		delim = text.charAt(2);
+	}
+	text = text.replaceAll("//" + delim + "\n", "");
+	
+	String[] numbers = text.split(",|\n|" + Character.toString(delim));
+	int sum = 0;
+	for(String d : numbers)
+    {
+   		sum += tonumber(d);
+   	}
+    return sum;
 }
 
 private static int tonumber(String text){
