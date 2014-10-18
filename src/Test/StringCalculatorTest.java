fb693e09d9d5334f3a718132f3a47d3c0f6b8d80
andriivars <andrii13@ru.is>
2014-10-18 15:14:50 +0000

    still trying

--- src/Test/StringCalculatorTest.java
+++ src/Test/StringCalculatorTest.java
@@ -1,5 +1,16 @@
 package Test;
 
-public class StringCalculatorTest {
 
+import static org.junit.Assert.*;
+
+import  edu.princeton.cs.introcs.*;
+import  edu.princeton.cs.algs4.*;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class StringCalculatorTest {
+@Test
+	public void test(){
+		Assert.assertEquals(0, 3);
+	}
 }
